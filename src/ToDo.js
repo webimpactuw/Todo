25b1842ccdc3d140a86c69437e21c6c119eca36e
Eric Xiao <42785168+mathlord2@users.noreply.github.com>
2023-02-12 21:25:54 -0800

    Update ToDo.js

--- src/ToDo.js
+++ src/ToDo.js
@@ -1,13 +1,24 @@
-import React from "react";
+// Import statements
 
 const ToDo = (props) => {
+    // Keeps track of the checked state of the to-do item
+    
+    // Changes the checked state of the to-do item
+    
     return (
         <div style={{
             border: "2px solid black",
             borderRadius: 15,
             width: "30vw",
-            margin: "10px auto"
+            margin: "10px auto",
+            display: "flex",
+            alignItems: "center"
         }}>
+            {checked ?
+                <IoCheckboxOutline size={30} style={{ "margin": "0px 10px" }} onClick={toggleChecked}/>
+                : 
+                <IoSquareOutline size={30} style={{ "margin": "0px 10px" }} onClick={toggleChecked}/>
+            }
             <h2>{props.text}</h2>
         </div>
     );
