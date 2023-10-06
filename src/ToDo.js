81b5dc43e8d039cbe5105035de9ad761d9172021
webimpact-uw <141550075+webimpact-uw@users.noreply.github.com>
2023-10-05 21:29:18 -0700

    Removed react icon dependency

--- src/ToDo.js
+++ src/ToDo.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { IoCheckboxOutline } from "react-icons/io5";
 
 const ToDo = (props) => {
     return (
@@ -7,11 +6,8 @@ const ToDo = (props) => {
             border: "2px solid black",
             borderRadius: 15,
             width: "30vw",
-            margin: "10px auto",
-            display: "flex",
-            alignItems: "center"
+            margin: "10px auto"
         }}>
-            <IoCheckboxOutline size={30} style={{ "margin": "0px 10px" }}/>
             <h2>{props.text}</h2>
         </div>
     );
